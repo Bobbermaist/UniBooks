88c041bea7f55c381fc5dabf4d42ad71ee851bed
Emiliano Bovetti <emilianobovetti@hotmail.it>
2013-12-03 12:40:48 +0100

    test controller

---
+++ application/controllers/test.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('head');
+		$this->load->view('body');
+
+		$this->load->model('User_model');
+		if( $this->User_model->exists_user('bob') === FALSE )
+			echo 'FALSE';
+		else
+			echo 'TRUE';
+
+		$this->load->view('coda');
+	}
+}
+
+/* End of file test.php */
+/* Location: ./application/controllers/test.php */ 
+ 
