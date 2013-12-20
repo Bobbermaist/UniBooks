3fc026f708a068de6bc94b26d56b5ec904852d3f
Emiliano Bovetti <emilianobovetti@hotmail.it>
2013-12-20 13:25:35 +0100

    updated form_data

--- application/config/form_data.php
+++ application/config/form_data.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array(
-
+    /* Registration form */
   'signup_data' => array(
   	'user_name_data' => array(
   		'name'			=> 'user_name',
@@ -25,32 +25,53 @@ $config = array(
   		'required'	=> TRUE*/
   	)
   ),
-
+    /* Reset password */
   'reset_data' => array(
   	'reset_form_data' => array(
   		'name'			=> 'user_or_email',
   		'maxlength'	=> '64'
   	)
   ),
-
   'new_password_data' => array(
   	'new_password_form_data' => array(
   		'name'			=> 'pass',
   		'maxlength'	=> '64'
   	)
   ),
-
+    /* Login form */
   'login_data' => array(
-  	'user_name' => array(
+  	'user_name_data' => array(
   		'name'			=> 'user_name',
   		'maxlength'	=> '20'
   	),
-  	'pass' => array(
+  	'pass_data' => array(
   		'name'			=> 'pass',
   		'maxlength'	=> '64'
   	)
+  ),
+    /* Account change */
+  'account_change_data' => array(
+    'user_name_data' => array(
+      'name'      => 'user_name',
+      'maxlength' => '20'
+    ),
+    'email_data' => array(
+      'name'      => 'email',
+      'maxlength' => '64'
+    ),
+    'old_pass_data' => array(
+      'name'      => 'old_pass',
+      'maxlength' => '64'
+    ),
+    'new_pass_data' => array(
+      'name'      => 'new_pass',
+      'maxlength' => '64'
+    ),
+    'passconf_data' => array(
+      'name'      => 'passconf',
+      'maxlength' => '64'
+    )
   )
-  
 );
 
 /* End of file form_data.php */
--- application/views/form/login.php
+++ application/views/form/login.php
@@ -1,8 +1,8 @@
 	<?php echo form_open('user/login'); ?>
 	  
 	  <h1>Login</h1>
-	  <p>Nickname: <?php echo form_input($user_name); ?> </p>
-	  <p>Password: <?php echo form_password($pass); ?> </p>
+	  <p>Nickname: <?php echo form_input($user_name_data); ?> </p>
+	  <p>Password: <?php echo form_password($pass_data); ?> </p>
 	<?php echo form_submit('login', 'Log In!');
 echo form_close();
 
