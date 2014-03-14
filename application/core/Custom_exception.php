bc9b524cc897990803b11df1bc727c0ed7a1e3db
Emiliano Bovetti <emilianobovetti@hotmail.it>
2014-03-14 22:29:37 +0100

    models documentation

--- application/core/Custom_exception.php
+++ application/core/Custom_exception.php
@@ -1,12 +1,40 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * UniBooks
+ *
+ * An application for books trade off
+ *
+ * @package UniBooks
+ * @author Emiliano Bovetti
+ * @since Version 1.0
+ */
 
+/**
+ * UniBooks Custom_exception class.
+ *
+ * Extends native PHP Exception class.
+ * The Custom exception can be constructed
+ * with simply the error code.
+ *
+ * All error codes are defined as contants in
+ * ./application/config/constants.php
+ * 
+ * The custom exception class will construct
+ * the parent Exception class with this error
+ * code and the message retrieved by 'exception_lang'
+ * file.
+ *
+ * @package UniBooks
+ * @category Models
+ * @author Emiliano Bovetti
+ */
 class Custom_exception extends Exception {
 
 	public function __construct($code, Exception $previous = NULL)
 	{
 		$code = (int) $code;
 		
-		parent::__construct($this->_get_message, $code, $previous);
+		parent::__construct($this->_get_message($code), $code, $previous);
 	}
 
 	/**
